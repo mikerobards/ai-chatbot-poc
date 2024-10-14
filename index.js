a8c3111cc95bd938b3f50a1de5c049f1c7f82b25
Michael <mikerobards@icloud.com>
2024-10-14 13:47:34 -0400

    add history capacity

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const reply = document.querySelector('.reply');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    main(input.value);
-    input.value = '';
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  main(input.value);
+  input.value = '';
 });
 
 async function main(input) {
@@ -17,8 +17,8 @@ async function main(input) {
     const match = await findNearestMatch(embedding);
     await getChatCompletion(match, input);
   } catch (error) {
-     console.error('Error in main function.', error.message);
-     reply.innerHTML = "Sorry, something went wrong. Please try again.";
+    console.error('Error in main function.', error.message);
+    reply.innerHTML = "Sorry, something went wrong. Please try again.";
   }
 }
 
@@ -38,7 +38,7 @@ async function findNearestMatch(embedding) {
     match_threshold: 0.50,
     match_count: 4
   });
-  
+
   // Manage multiple returned matches
   const match = data.map(obj => obj.content).join('\n');
   return match;
@@ -46,8 +46,8 @@ async function findNearestMatch(embedding) {
 
 // Use OpenAI to make the response conversational
 const chatMessages = [{
-    role: 'system',
-    content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given two pieces of information - some context about movies and a question. Your main job is to formulate a short answer to the question using the provided context. If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.` 
+  role: 'system',
+  content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given two pieces of information - some context about movies and a question. Your main job is to formulate a short answer to the question using the provided context. If the answer is not given in the context, find the answer in the conversation history if possible. If you are unsure and cannot find the answer, say, "Sorry, I don't know the answer." Please do not make up the answer. Always speak as if you were chatting to a friend.`
 }];
 
 async function getChatCompletion(text, query) {
@@ -55,12 +55,13 @@ async function getChatCompletion(text, query) {
     role: 'user',
     content: `Context: ${text} Question: ${query}`
   });
-  
+
   const { choices } = await openai.chat.completions.create({
     model: 'gpt-4',
     messages: chatMessages,
     temperature: 0.65,
     frequency_penalty: 0.5
   });
+  chatMessages.push(choices[0].message);
   reply.innerHTML = choices[0].message.content;
 }
